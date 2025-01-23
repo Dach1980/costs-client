6a31e18fe036ea3a79703bb06dbc94d54f9e0f82
Dmitry Cherkashin <dchpost@mail.ru>
2025-01-24 07:52:03 +1000

    добавляет навигацию страниц

---
+++ src/components/AuthPage/AuthPage.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.css'
+
+export const AuthPage = ({ type }: { type: 'login' | 'registration' }) => {
+    const currentAuthTitle = type === 'login' ? 'Войти' : 'Регистрация';
+
+    return (
+        <div className='container'>
+            <h1>{currentAuthTitle}</h1>
+            <form className='form-group'>
+                <label className='auth-label'>
+                    Введите имя пользователя
+                    <input type="text" className='form-control' />
+                </label>
+                <label className='auth-label'>
+                    Введите пароль
+                    <input type="text" className='form-control' />
+                </label>
+                <button className='btn btn-primary auth-btn'>{currentAuthTitle}</button>
+            </form>
+            {type == 'login'
+                ? <div>
+                    <span className='question-text'>Еще нет аккаунта?</span>
+                    <Link to={'/registration'}>Зарегистрироваться</Link>
+                </div>
+                : <div>
+                    <span className='question-text'>Уже есть аккаут?</span>
+                    <Link to={'/login'}>Войти</Link>
+                </div>
+            }
+        </div>
+    )
+}
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,7 +3,7 @@ import {useState, useEffect} from 'react'
 export const useTheme = () => {
     const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme') as string) || 'dark')
     const darkTheme = 'https://cdn.jsdelivr.net/npm/@forevolve/bootstrap-dark@1.0.0/dist/css/bootstrap-dark.min.css';
-    const lightTheme = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
+    const lightTheme = 'https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css';
 
     const setCurrentMode = (theme: string) => {
         const link = document.getElementById('theme-link') as HTMLLinkElement;
