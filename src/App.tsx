e682ba6457bd7db3030835bd0f689baef6109bc4
Dmitry Cherkashin <dchpost@mail.ru>
2025-01-24 11:07:19 +1000

    созданы страницы login и registration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import {useStore} from 'effector-react'
+import { useUnit} from 'effector-react'
 import Header from "./components/Header/Header.tsx";
 import { AuthPage } from './components/AuthPage/AuthPage.tsx';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Navigate, Route, BrowserRouter, Routes } from 'react-router-dom';
 import { $auth } from './context/auth.ts';
 
 function App() {
-  const isLoggedIn = useStore($auth);
+  const isLoggedIn = useUnit($auth);
 
   return (
     <div className="App">
       <Header />
-      <Router>
+      <BrowserRouter>
         <Routes>
-          <Route path='/' element={isLoggedIn ? <Navigate to={'/cost'}/> : <Navigate to='login'/>}/>
-          <Route path='/registration' element={<AuthPage type='registration'/>}/>
-          <Route path='/login' element={<AuthPage type='login'/>}/>
-          <Route path='/costs' element={<h1>Costs</h1>}/>
+          <Route path='/' element={isLoggedIn ? <Navigate to='/cost'/> : <Navigate to='login'/>}/>
+          <Route path='/registration' element={isLoggedIn ? <Navigate to='/cost'/> : <AuthPage type='registration'/>}/>
+          <Route path='/login' element={isLoggedIn ? <Navigate to='/cost'/> : <AuthPage type='login'/>}/>
+          <Route path='/costs' element={isLoggedIn ? <h1>Costs</h1> : <Navigate to='/login'/>}/>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/AuthPage/AuthPage.tsx
+++ src/components/AuthPage/AuthPage.tsx
@@ -1,34 +1,96 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useRef, MutableRefObject } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import './styles.css'
+import { AuthClient } from '../../api/authClient.ts';
+import { Spinner } from './../Spinner/Spinner.tsx'
 
 export const AuthPage = ({ type }: { type: 'login' | 'registration' }) => {
-    const currentAuthTitle = type === 'login' ? 'Войти' : 'Регистрация';
+  const [spinner, setSpinner] = useState(false);
+  const usernameRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const passwordRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const navigate = useNavigate();
+  const currentAuthTitle = type === 'login' ? 'Войти' : 'Регистрация';
 
-    return (
-        <div className='container'>
-            <h1>{currentAuthTitle}</h1>
-            <form className='form-group'>
-                <label className='auth-label'>
-                    Введите имя пользователя
-                    <input type="text" className='form-control' />
-                </label>
-                <label className='auth-label'>
-                    Введите пароль
-                    <input type="text" className='form-control' />
-                </label>
-                <button className='btn btn-primary auth-btn'>{currentAuthTitle}</button>
-            </form>
-            {type == 'login'
-                ? <div>
-                    <span className='question-text'>Еще нет аккаунта?</span>
-                    <Link to={'/registration'}>Зарегистрироваться</Link>
-                </div>
-                : <div>
-                    <span className='question-text'>Уже есть аккаут?</span>
-                    <Link to={'/login'}>Войти</Link>
-                </div>
-            }
+  const handleLogin = async (username: string, password: string) => {
+    if (!username || !password) {
+      return;
+    }
+    const result = await AuthClient.login(username, password);
+
+    if (!result) {
+      setSpinner(false);
+      return;
+    }
+
+    setSpinner(false);
+    navigate('/costs');
+    // setAlert({ alertText: 'Вход выполнен', alertStatus: 'success' });
+  }
+
+  const handleRegistration = async (username: string, password: string) => {
+    if (!username || !password) {
+      return;
+    }
+
+    if (password.length < 4) {
+      return;
+    }
+
+    const result = await AuthClient.registration(username, password);
+
+    if (!result) {
+      setSpinner(false);
+      return;
+    }
+
+    setSpinner(false);
+    navigate('/login');
+    // setAlert({ alertText: 'Регистрация выполнена', alertStatus: 'success' });
+  }
+
+  const handleAuth = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setSpinner(true);
+
+    switch (type) {
+      case 'login':
+        handleLogin(usernameRef.current.value, passwordRef.current.value);
+        break;
+      case 'registration':
+        handleRegistration(usernameRef.current.value, passwordRef.current.value);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return (
+    <div className='container'>
+      <h1>{currentAuthTitle}</h1>
+      <form onSubmit={handleAuth} className='form-group' >
+        <label className='auth-label'>
+          Введите имя пользователя
+          <input ref={usernameRef} type="text" className='form-control' />
+        </label>
+        <label className='auth-label'>
+          Введите пароль
+          <input ref={passwordRef} type="text" className='form-control' />
+        </label>
+        <button className='btn btn-primary auth-btn'>
+          {spinner ? <Spinner top={5} left={20} /> : currentAuthTitle}
+        </button>
+      </form>
+      {type === 'login'
+        ? <div>
+          <span className='question-text'>Еще нет аккаунта?</span>
+          <Link to={'/registration'}>Зарегистрироваться</Link>
         </div>
-    )
+        : <div>
+          <span className='question-text'>Уже есть аккаут?</span>
+          <Link to={'/login'}>Войти</Link>
+        </div>
+      }
+    </div>
+  )
 }
